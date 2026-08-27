fd51a70c0b1b7eac6e77c6ba071277e1f3c63732
Lipe1042 <felipeoliveir27@gmail.com>
2026-08-27 01:24:18 -0300

    Configura funções de criar, salvar e listar anotações

--- banco.py
+++ banco.py
@@ -1,9 +1,31 @@
 import sqlite3
 
-conn = sqlite3.connect("caderno.db")
-cursor = conn.cursor()
+conexao = sqlite3.connect("banco.db")
+cursor = conexao.cursor()
 
 
 
+def criar_tabela():
+        cursor.execute("""
+                CREATE TABLE IF NOT EXISTS anotacoes (
+                id INTEGER PRIMARY KEY,
+                titulo TEXT NOT NULL,
+                conteudo TEXT NOT NULL
+                )
+        """)
 
-cursor.execute("CREATE TABLE sintaxes (id INTEGER, termo TEXT)")
+
+def salvar_anotacao(titulo, conteudo):
+        cursor.execute("""
+                INSERT INTO anotacoes  
+                (titulo, conteudo)
+                VALUES (?, ?)""", (titulo, conteudo))
+        conexao.commit()
+
+def listar_anotacoes():
+        cursor.execute("""SELECT * FROM anotacoes""")
+
+        textos = cursor.fetchall()
+        return textos
+        
+
